2ac54c23d2e1e7139f2613bc52a7f4b93db911e4
TruthSearchers <40608534+TruthSearchers@users.noreply.github.com>
2024-10-17 14:43:34 +0530

    Update before_after.js

--- before_after.js
+++ before_after.js
@@ -2,7 +2,6 @@ const slider = document.querySelector('.slider');
 const handle = document.querySelector('.handle');
 const whiteLine = document.querySelector('.white-line');
 const afterImage = document.querySelector('.after');
-
 let isDragging = false;
 let autoSlideInterval;
 let autoSlideActive = true;
@@ -11,7 +10,7 @@ function moveSlider(xPosition) {
   const sliderWidth = slider.offsetWidth;
   const position = Math.min(Math.max(0, xPosition), sliderWidth);
   const percentage = (position / sliderWidth) * 100;
-  
+ 
   handle.style.left = `${percentage}%`;
   whiteLine.style.left = `${percentage}%`;
   afterImage.style.clipPath = `inset(0 ${100 - percentage}% 0 0)`;
@@ -22,7 +21,9 @@ function startDrag(e) {
   isDragging = true;
   clearInterval(autoSlideInterval);
   autoSlideActive = false;
-  e.preventDefault(); // Prevent image selection during drag
+  if (e.cancelable) {
+    e.preventDefault(); // Prevent image selection during drag
+  }
 }
 
 function stopDrag() {
@@ -53,32 +54,31 @@ whiteLine.addEventListener('mousedown', startDrag);
 window.addEventListener('mousemove', duringDrag);
 window.addEventListener('mouseup', stopDrag);
 
-// Event listeners for touch events (for mobile)
-handle.addEventListener('touchstart', startDrag);
-whiteLine.addEventListener('touchstart', startDrag);
-window.addEventListener('touchmove', duringDrag);
+// Event listeners for touch events (for mobile) with passive option
+handle.addEventListener('touchstart', startDrag, { passive: false });
+whiteLine.addEventListener('touchstart', startDrag, { passive: false });
+window.addEventListener('touchmove', duringDrag, { passive: true });
 window.addEventListener('touchend', stopDrag);
 
 // Automatic sliding functionality
 function startAutoSlide() {
   if (autoSlideActive) return; // Prevent restarting if already active
-  
+ 
   let direction = 1;
   autoSlideInterval = setInterval(() => {
     let handlePosition = handle.offsetLeft + direction * 2; // Adjust speed
     if (handlePosition >= slider.offsetWidth || handlePosition <= 0) {
       direction *= -1; // Reverse direction
     }
-    
+   
     moveSlider(handlePosition);
   }, 50*Math.sqrt(design_width/custom_width)); // Adjust the speed of movement
-
   autoSlideActive = true;
 }
 
 // Stop auto-slide when user interacts
 handle.addEventListener('mousedown', () => clearInterval(autoSlideInterval));
-handle.addEventListener('touchstart', () => clearInterval(autoSlideInterval));
+handle.addEventListener('touchstart', () => clearInterval(autoSlideInterval), { passive: true });
 window.addEventListener('mouseup', startAutoSlide);
 window.addEventListener('touchend', startAutoSlide);
 
