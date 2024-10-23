890ee416961bfecdfacad71c9054f32d24123278
TruthSearchers <40608534+TruthSearchers@users.noreply.github.com>
2024-10-23 10:54:48 +0530

    Update formfinal.js

--- formfinal.js
+++ formfinal.js
@@ -103,11 +103,6 @@ const questions = [
   }
 ];
 
-const CONFIG = {
-    RAZORPAY_KEY: process.env.RAZORPAY_KEY || '',
-    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || ''
-};
-
 // Modified loadChatState function
 async function loadChatState() {
   const savedState = localStorage.getItem('chatState');
